55b4048dbfbdc35ee369f6eaa45959e3a2b334e3
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-04-19 23:57:45 +0300

    Typing lists

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Define function to sum all elements in a list'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''Sum all elements in input_list'''
+    return sum(input_list)
