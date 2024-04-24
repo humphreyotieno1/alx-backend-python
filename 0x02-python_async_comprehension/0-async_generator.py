6b2fb98ca122dd9061f43719c813c3f0720d34f7
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-04-24 12:32:29 +0300

    Correction

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import random
 import typing
 
 
-async def async_generator() -> typing.AsyncGenerator[float, None, None]:
+async def async_generator() -> None: # type: ignore
     '''Asynchronous generator that yields a random number'''
     for _ in range(10):
         await asyncio.sleep(1)
