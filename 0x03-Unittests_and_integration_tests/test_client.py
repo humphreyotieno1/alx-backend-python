d6129cca2b9048c2424fbea147d53aa4aaa55157
humphreyotieno1 <humphreyotieno04@gmail.com>
2025-05-31 11:49:04 +0300

    Integration tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -95,56 +95,6 @@ class TestGithubOrgClient(unittest.TestCase):
 @parameterized_class(('org_payload', 'repos_payload', 'expected_repos', 'apache2_repos'), 
                     TEST_PAYLOAD)
 class TestIntegrationGithubOrgClient(unittest.TestCase):
-    """Integration test class for GithubOrgClient"""
-
-    @classmethod
-    def setUpClass(cls):
-        """Set up class with mock for requests.get"""
-        cls.get_patcher = patch('requests.get')
-        cls.mock_get = cls.get_patcher.start()
-
-        def side_effect(url):
-            """Side effect function to return different payloads based on URL"""
-            if "orgs/" in url:
-                return Mock(json=lambda: cls.org_payload)
-            if "repos" in url:
-                return Mock(json=lambda: cls.repos_payload)
-            return Mock(json=lambda: None)
-
-        cls.mock_get.side_effect = side_effect
-
-    @classmethod
-    def tearDownClass(cls):
-        """Stop the patcher"""
-        cls.get_patcher.stop()
-
-    def test_public_repos(self):
-        """Test public_repos returns expected repositories"""
-        client = GithubOrgClient("testorg")
-        repos = client.public_repos()
-        self.assertEqual(repos, self.expected_repos)
-
-    def test_public_repos_with_license(self):
-        """Test public_repos with license filter"""
-        client = GithubOrgClient("testorg")
-        repos = client.public_repos(license="apache-2.0")
-        self.assertEqual(repos, self.apache2_repos)
-
-@parameterized_class([
-    {
-        'org_payload': TEST_PAYLOAD[0][0],
-        'repos_payload': TEST_PAYLOAD[0][1],
-        'expected_repos': TEST_PAYLOAD[0][2],
-        'apache2_repos': TEST_PAYLOAD[0][3],
-    },
-    {
-        'org_payload': TEST_PAYLOAD[1][0],
-        'repos_payload': TEST_PAYLOAD[1][1],
-        'expected_repos': TEST_PAYLOAD[1][2],
-        'apache2_repos': TEST_PAYLOAD[1][3],
-    }
-])
-class TestIntegrationGithubOrgClient(unittest.TestCase):
     """Integration test class for GithubOrgClient.public_repos"""
 
     @classmethod
