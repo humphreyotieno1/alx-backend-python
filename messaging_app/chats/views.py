2564a68a770130c7d4398b407ae4dcc19f7eac13
humphreyotieno1 <humphreyotieno04@gmail.com>
2025-05-31 21:52:35 +0300

    Api endpoints

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,12 +1,30 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ConversationViewSet, MessageViewSet
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
-# Router and register our viewsets with it
-router = DefaultRouter()
-router.register(r'conversations', ConversationViewSet)
-router.register(r'messages', MessageViewSet, basename='message')
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-urlpatterns = [
-    path('', include(router.urls)),
-]
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+    
+    def get_queryset(self):
+        return Message.objects.all()
+    
+    @action(detail=False, methods=['post'])
+    def send_message(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
